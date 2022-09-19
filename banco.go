8f5999add5f0a3c0d9b737d85c69d778f6fec545
ZacSv <71881016+ZacSv@users.noreply.github.com>
2022-09-19 13:00:53 -0300

    implementação

--- banco.go
+++ banco.go
@@ -2,31 +2,15 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/ZacSv/projetosPessoais/contas"
 )
 
 func main() {
 
-	clienteIsac := contas.Cliente{}
-	clienteIsac.Cpf = "701.823.576-98"
-	clienteIsac.Titular = "Isac"
-	clienteIsac.Profissao = "Desenvolvedor"
-
 	contaIsac := contas.ContaCorrente{}
-	contaIsac.NomeTitular = clienteIsac
-	contaIsac.SaldoConta = 500.00
+	fmt.Println(contaIsac)
 
-	clienteKaryne := contas.Cliente{}
-	clienteIsac.Cpf = "701.823.576-78"
-	clienteIsac.Titular = "Karyne"
-	clienteIsac.Profissao = "Desenvolvedor"
-
-	contaKaryne := contas.ContaCorrente{}
-	contaKaryne.NomeTitular = clienteKaryne
-	contaKaryne.SaldoConta = 1200.00
-
-	fmt.Println(contaKaryne.Transferir(200, &contaIsac)) //"&" identifica o endereço na memoria da conta que recebe o dinheiro
-	fmt.Println(contaIsac.SaldoConta)
-	fmt.Println(contaKaryne.SaldoConta)
+	contaKaryne := contas.contaPoupanca{}
 
 }
--- contas/contaCorrente.go
+++ contas/contaCorrente.go
@@ -1,11 +1,39 @@
 package contas
 
-import (
-	"github.com/ZacSv/projetosPessoais/contas"
-)
-
 type contaPoupanca struct {
-	Titular       contas.Titular
+	Titular       string
 	NumeroAgencia int
 	NumeroConta   int
+	Operacao      int
+	saldo         float64
+}
+
+func (c *contaPoupanca) Sacar(valorDoSaque float64) (string, float64) { //Definindo uma função para sacar
+
+	podeSacar := valorDoSaque <= c.saldo && valorDoSaque >= 0 //Definindo como condiional para sacar o valor do saque ser menor ou igual ao valor da conta
+
+	if podeSacar { //Aplicando a condicional caso for verdadeira
+		c.saldo -= valorDoSaque                                         //Subtraia o valor
+		return "Saque realizado com sucesso, saldo atual de: ", c.saldo //Imprima o sucesso
+	} else {
+		return "Saldo insuficiente para o saque, ou saque menor do que 0", c.saldo //Caso a condicional seja falsa, imprima!
+	}
+
+}
+
+func (c *contaPoupanca) Depositar(valorDeposito float64) (string, float64) { //Definindo uma função para depositar
+
+	podeDepositar := valorDeposito > 0 //Definindo como condicional para depositar o valor do depósito ser maior do que 0
+
+	if podeDepositar { //Aplicando a condicional caso for verdadeira
+		c.saldo += valorDeposito                                           //Some o valor
+		return "Deposito realizado com sucesso, saldo atual de: ", c.saldo //Imprima o sucesso
+	} else {
+		return "O valor que está sendo depositado é imcompatível", c.saldo //Caso a condicional seja falsa, imprima!
+	}
+
+}
+
+func (c *contaPoupanca) ObterSaldo() float64 {
+	return c.saldo
 }
